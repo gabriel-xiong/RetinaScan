fa6eb7824d1d6b9bfd800d2944f74700a9be7f20
Gabriel <gpdxiong@gmail.com>
2024-07-02 12:00:00 -0500

    add training convenience scripts for each lesion type

---
+++ scripts/train_microaneurysms.ps1
@@ -0,0 +1,8 @@
+param(
+    [Parameter(Mandatory = $true)] [string] $DatasetRoot
+)
+
+python -m diabetic_retinopathy.training.train_sam `
+    --image-dir "$DatasetRoot\A. Segmentation\1. Original Images\a. Training Set" `
+    --mask-dir "$DatasetRoot\A. Segmentation\2. All Segmentation Groundtruths\a. Training Set\1. Microaneurysms" `
+    --output-path "checkpoints\microaneurysms.pth"
